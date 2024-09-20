7a98cb330943f6ca4cc0c821c53e9c9674867613
Lilianperim <liliancrisperim@gmail.com>
2024-09-20 14:32:08 -0300

    Criando banco de dados

---
+++ lib/util/db_helper.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DBHelper {
+  static final DBHelper instance = DBHelper._internal();
+
+  final String tblBook = "book";
+  final String colId = "id";
+  final String colTitle = "title";
+  final String colGenre = "genre";
+  final String colAuthor = "author";
+
+  static Database? _db;
+
+  DBHelper._internal();
+
+  factory DBHelper() {
+    return instance;
+  }
+
+  Future<Database> initializeDb() async {
+    Directory dir = await getApplicationDocumentsDirectory();
+    String path = join(dir.path, "books.db");
+    var dbBooks = await openDatabase(path, version: 1, onCreate: _createDb);
+    return dbBooks;
+  }
+
+  void _createDb(Database db, int newVersion) async {
+    await db.execute(
+        "CREATE TABLE $tblBook($colId INTEGER PRIMARY KEY, $colTitle TEXT, " +
+            "$colGenre TEXT, $colAuthor TEXT)");
+  }
+
+  Future<Database> get db async {
+    _db ??= await initializeDb();
+    return _db!;
+  }
+
+  Future<List<Map<String, dynamic>>> getBooks() async {
+    Database db = await this.db;
+    return await db.rawQuery('SELECT * FROM $tblBook');
+  }
+}
